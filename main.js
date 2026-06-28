899864f504cb9bc768537772567ee03616613c08
2470369094-boop <2470369094@qq.com>
2026-06-28 22:23:56 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// 黑曜石-于 插件基础模板
+module.exports = class YuPlugin {模块.导出 = 类 YuPlugin {
+  onload() {  加载() {
+    console.log("黑曜石-于 插件加载成功");    控制台.日志("黑曜石-于 插件加载成功");
+  }
+  onunload() {
+    console.log("黑曜石-于 插件卸载");
+  }
+           }
